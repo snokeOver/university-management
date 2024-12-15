5247c57e20db8c9a215efeca9eddde7f764bc35a
snokeOver <snokeover@gmail.com>
2024-12-15 23:37:43 +0600

    Implemented transaction-rollback at crate new user and student

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 //This is the business logic
 
+import mongoose from "mongoose";
 import { defPass } from "../..";
 import { AcademicSemesterMoel } from "../academicSemester/academicSemester.model";
 import { IStudent } from "../student/student.interface";
@@ -7,35 +8,57 @@ import { StudentModel } from "../student/student.model";
 import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 import { generateStudentId } from "./user.utils";
+import { AppError } from "../../middlewares/errorHandler";
+
+// import { status } from "http-status";
 
 // Create a student data
 export const createStudentToDB = async (
   credentials: Partial<IUser>,
   student: IStudent
 ) => {
-  const fetchedSem = await AcademicSemesterMoel.findById(
-    student.academicSemister
-  );
+  const session = await mongoose.startSession();
 
-  if (!fetchedSem) throw new Error("Academic Semester not found!");
+  try {
+    session.startTransaction();
 
-  const newUser: Partial<IUser> = {
-    id: await generateStudentId(fetchedSem),
-    password: credentials.password || defPass,
-    email: credentials.email,
-    role: "Student",
-  };
+    const fetchedSem = await AcademicSemesterMoel.findById(
+      student.academicSemister,
+      null,
+      { session }
+    );
 
-  const createdUser = await UserModel.create(newUser);
+    if (!fetchedSem) throw new Error("Academic Semester not found!");
 
-  if (createdUser) {
-    student.studentId = createdUser.id;
-    student.userId = createdUser._id;
-  } else {
-    throw new Error("Failed to create user");
+    const newUser: Partial<IUser> = {
+      id: await generateStudentId(fetchedSem),
+      password: credentials.password || defPass,
+      email: credentials.email,
+      role: "Student",
+    };
+
+    const createdUser = await UserModel.create([newUser], { session });
+
+    if (!createdUser.length)
+      throw new AppError(509, "Bad Request", "Failed to create user");
+
+    student.studentId = createdUser[0].id;
+    student.userId = createdUser[0]._id;
+
+    const result = await StudentModel.create([student], { session });
+
+    if (!result.length)
+      throw new AppError(509, "Bad Request", "Failed to create new user");
+
+    result[0].id = createdUser[0].id;
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
-
-  const result = await StudentModel.create(student);
-  result.id = createdUser.id;
-  return result;
 };
